5296c7ce68b2396c96a161bda309d881219bc788
Songmin Li <lisongmin@protonmail.com>
2023-06-26 20:49:46 +0800

    Save official metallb kustomization config locally

---
+++ apps/metallb/update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+METALLB_VERSION=${METALLB_VERSION:-0.13.10}
+
+LOCAL_DIR=$(dirname $0)
+CONTENT_URL="https://github.com/metallb/metallb/config/frr?ref=v${METALLB_VERSION}"
+
+pushd "$LOCAL_DIR/origin"
+
+config_version=$(cat .config_version 2>/dev/null)
+if [ "$METALLB_VERSION" = "$config_version" ]; then
+	echo "Current config version is already $METALLB_VERSION, skip update"
+	exit 0
+fi
+
+kustomize build "$CONTENT_URL" -o frr-config.yml || exit 1
+echo "$METALLB_VERSION" >.config_version
+
+popd
