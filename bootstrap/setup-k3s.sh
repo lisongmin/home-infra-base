10bd543cea9b451a1ba51a25714a3eab0093cd6a
Songmin Li <lisongmin@protonmail.com>
2023-06-27 08:59:46 +0800

    Add script to setup sops environment

--- bootstrap/setup-k3s.sh
+++ bootstrap/setup-k3s.sh
@@ -9,49 +9,49 @@ K3S_SERVERS_TO_JOIN=${K3S_SERVERS_TO_JOIN:-}
 K3S_AGENTS=${K3S_AGENTS:-}
 
 if [ -n "$K3S_CLUSTER_CIDR_V4" -a -n "$K3S_CLUSTER_CIDR_V6"]; then
-	K3S_CLUSTER_CIDR="${K3S_CLUSTER_CIDR_V4},${K3S_CLUSTER_CIDR_V6}"
-	K3S_SERVICE_CIDR="${K3S_SERVICE_CIDR_V4},${K3S_SERVICE_CIDR_V6}"
+  K3S_CLUSTER_CIDR="${K3S_CLUSTER_CIDR_V4},${K3S_CLUSTER_CIDR_V6}"
+  K3S_SERVICE_CIDR="${K3S_SERVICE_CIDR_V4},${K3S_SERVICE_CIDR_V6}"
 elif [ -n "$K3S_CLUSTER_CIDR_V4" ]; then
-	K3S_CLUSTER_CIDR="${K3S_CLUSTER_CIDR_V4}"
-	K3S_SERVICE_CIDR="${K3S_SERVICE_CIDR_V4}"
+  K3S_CLUSTER_CIDR="${K3S_CLUSTER_CIDR_V4}"
+  K3S_SERVICE_CIDR="${K3S_SERVICE_CIDR_V4}"
 elif [ -n "$K3S_CLUSTER_CIDR_V6" ]; then
-	K3S_CLUSTER_CIDR="${K3S_CLUSTER_CIDR_V6}"
-	K3S_SERVICE_CIDR="${K3S_SERVICE_CIDR_V6}"
+  K3S_CLUSTER_CIDR="${K3S_CLUSTER_CIDR_V6}"
+  K3S_SERVICE_CIDR="${K3S_SERVICE_CIDR_V6}"
 else
-	K3S_CLUSTER_CIDR=10.42.0.0/16
-	K3S_SERVICE_CIDR=10.43.0.0/16
+  K3S_CLUSTER_CIDR=10.42.0.0/16
+  K3S_SERVICE_CIDR=10.43.0.0/16
 fi
 
 K3S_EXTRA_ARGS="$K3S_EXTRA_ARGS --flannel-backend=wireguard-native --disable=servicelb,traefik --cluster-cidr=${K3S_CLUSTER_CIDR} --service-cidr=${K3S_SERVICE_CIDR}"
 
 k3sup install \
-	--cluster \
-	--user "${K3S_USER}" \
-	--host "${K3S_SERVER}" \
-	--k3s-extra-args "${K3S_EXTRA_ARGS}" \
-	--k3s-version "${K3S_VERSION}" || exit $?
+  --cluster \
+  --user "${K3S_USER}" \
+  --host "${K3S_SERVER}" \
+  --k3s-extra-args "${K3S_EXTRA_ARGS}" \
+  --k3s-version "${K3S_VERSION}" || exit $?
 
 if [ -n "${K3S_SERVERS_TO_JOIN}" ]; then
-	for server_to_join in "${K3S_SERVERS_TO_JOIN[@]}"; do
-		k3sup join \
-			--server \
-			--server_user "${K3S_USER}" \
-			--server_host "${K3S_SERVER}" \
-			--user "${K3S_USER}" \
-			--host "${server_to_join}" \
-			--k3s-extra-args "${K3S_EXTRA_ARGS}" \
-			--k3s-version "${K3S_VERSION}" || exit $?
-	done
+  for server_to_join in "${K3S_SERVERS_TO_JOIN[@]}"; do
+    k3sup join \
+      --server \
+      --server_user "${K3S_USER}" \
+      --server_host "${K3S_SERVER}" \
+      --user "${K3S_USER}" \
+      --host "${server_to_join}" \
+      --k3s-extra-args "${K3S_EXTRA_ARGS}" \
+      --k3s-version "${K3S_VERSION}" || exit $?
+  done
 fi
 
 if [ -n "${K3S_AGENTS}" ]; then
-	for agent in "${K3S_AGENTS[@]}"; do
-		k3sup join \
-			--server_user "${K3S_USER}" \
-			--server_host "${K3S_SERVER}" \
-			--user "${K3S_USER}" \
-			--host "${agent}" \
-			--k3s-extra-args "${K3S_EXTRA_ARGS}" \
-			--k3s-version "${K3S_VERSION}" || exit $?
-	done
+  for agent in "${K3S_AGENTS[@]}"; do
+    k3sup join \
+      --server_user "${K3S_USER}" \
+      --server_host "${K3S_SERVER}" \
+      --user "${K3S_USER}" \
+      --host "${agent}" \
+      --k3s-extra-args "${K3S_EXTRA_ARGS}" \
+      --k3s-version "${K3S_VERSION}" || exit $?
+  done
 fi
---
+++ bootstrap/setup-sops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Setup a sops environment in the current directory so that it will
+# encrypt the data or stringData fields in the files which end with
+# .secret.yml or .secret.yaml
+
+SOPS_PATH=${SOPS_PATH:-~/.config/sops/homelab}
+mkdir -p "${SOPS_PATH}"
+
+if [ ! -e "${SOPS_PATH}/age.key" ]; then
+  age-keygen -o "${SOPS_PATH}/age.key" || exit $?
+fi
+
+if [ ! -e .sops.yaml ]; then
+  public_key=$(cat ${SOPS_PATH}/age.key | sed -n 's/^# public key: \(\w\+\)$/\1/p')
+
+  cat <<EOF >.sops.yaml
+  ---
+  creation_rules:
+  - encrypted_regex: '^(data|stringData)$'
+    path_regex: '.*\/.*\.secret\.ya?ml$'
+    age: >-
+      ${public_key}
+EOF
+fi
