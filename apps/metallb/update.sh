9d8404548a01a6f2c8afe6d630f5d979250a5468
Songmin Li <lisongmin@protonmail.com>
2023-07-01 12:01:57 +0800

    Fix shellcheck warning

--- apps/metallb/update.sh
+++ apps/metallb/update.sh
@@ -2,10 +2,10 @@
 
 METALLB_VERSION=${METALLB_VERSION:-0.13.10}
 
-LOCAL_DIR=$(dirname $0)
+LOCAL_DIR=$(dirname "$0")
 CONTENT_URL="https://github.com/metallb/metallb/config/frr?ref=v${METALLB_VERSION}"
 
-pushd "$LOCAL_DIR/origin"
+pushd "$LOCAL_DIR/origin" || exit $?
 
 config_version=$(cat .config_version 2>/dev/null)
 if [ "$METALLB_VERSION" = "$config_version" ]; then
@@ -16,4 +16,4 @@ fi
 kustomize build "$CONTENT_URL" -o frr-config.yml || exit 1
 echo "$METALLB_VERSION" >.config_version
 
-popd
+popd || exit $?
