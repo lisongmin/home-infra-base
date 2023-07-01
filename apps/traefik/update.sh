923698182c9c4f13abd8407716d1a6541bb7cf97
Songmin Li <lisongmin@protonmail.com>
2023-07-01 11:59:29 +0800

    Change traefik image version on update origin crds

--- apps/traefik/update.sh
+++ apps/traefik/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TRAEFIK_VERSION=${TRAEFIK_VERSION:-2.10}
+TRAEFIK_VERSION=${TRAEFIK_VERSION:-2.10.3}
 
-LOCAL_DIR=$(dirname $0)
+LOCAL_DIR=$(dirname "$0")
 CONTENT_URL=https://raw.githubusercontent.com/traefik/traefik/v${TRAEFIK_VERSION}/docs/content/reference/dynamic-configuration
 
 INGRESS_CONFIGURATIONS=(kubernetes-crd-definition-v1.yml
@@ -14,7 +14,7 @@ GATEWAY_CONFIGURATIONS=(gateway.networking.k8s.io_gatewayclasses.yaml
 	gateway.networking.k8s.io_tlsroutes.yaml
 	kubernetes-gateway-rbac.yml)
 
-pushd "${LOCAL_DIR}/origin"
+pushd "${LOCAL_DIR}/origin" || exit $?
 
 # ingress configurations
 for config in "${INGRESS_CONFIGURATIONS[@]}"; do
@@ -28,4 +28,10 @@ for config in "${GATEWAY_CONFIGURATIONS[@]}"; do
 	wget -q -O "${config}" "${CONTENT_URL}/${config}" || exit $?
 done
 
-popd
+popd || exit $?
+
+pushd "$LOCAL_DIR" || exit $?
+
+sed -i 's/traefik:v[0-9.]\+/traefik:v'"$TRAEFIK_VERSION/g" traefik.yml
+
+popd || exit $?
